a99c3a7100d9e4ed6caad84002f5c11ed3f8d0ea
RubenMRDev <rubenmartinruiz05@gmail.com>
2025-02-18 09:58:31 +0100

    html and css done

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-const
+const canvas = document.getElementById('tetris');
+const ctx = canvas.getContext('2d');
+const pauseBtn = document.getElementById('pauseButton');
+const score = document.getElementById('score');
+const highScore = document.getElementById('highScore');
+
